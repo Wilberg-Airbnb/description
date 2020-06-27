01a1a4041cc696aac3fecc1e08cf16516818900a
Eric Zepeda <zepedaeric@gmail.com>
2020-06-26 17:48:16 -0700

    Work on descriptions widget conditionally rendering

--- client/components/MainDescription.jsx
+++ client/components/MainDescription.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const MainDescription = ({ description }) => {
-  return <h2>Hey here it is!</h2>;
-};
+class MainDescription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMore: false,
+    };
+    // this.showMore = this.showMore.bind(this);
+  }
+
+  showMore(event) {
+    this.setState({ showMore: true });
+  }
+
+  render() {
+    let sections = this.props.descriptions;
+    let reactSections = sections.map((section, i) => {
+      return (
+        <section key={i}>
+          <h6>{section.header}</h6>
+          <p>{section.description}</p>
+        </section>
+      );
+    });
+    if (!this.state.showMore) {
+      return (
+        <div>
+          {reactSections.slice(0, 1)}
+          <span id="seeMore" onClick={() => this.showMore()}>
+            ...read More
+          </span>
+        </div>
+      );
+    } else {
+      return <div>{reactSections}</div>;
+    }
+  }
+}
 
 export default MainDescription;
