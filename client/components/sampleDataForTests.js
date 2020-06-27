49b294156f0cab4addbd3d0f0da591a69722d604
Eric Zepeda <zepedaeric@gmail.com>
2020-06-26 18:39:58 -0700

    Create sample object

--- client/components/sampleDataForTests.js
+++ client/components/sampleDataForTests.js
@@ -16,4 +16,61 @@ const sampleDescriptions = [
   },
 ];
 
-export { sampleDescriptions };
+const amenities = [
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Bed linens',
+    amenityDescription: '',
+    subheader: 'Bed and bath',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Iron',
+    amenityDescription: '',
+    subheader: 'Basic',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Carbon monoxide alarm',
+    amenityDescription: '',
+    subheader: 'Safety features',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Hangers',
+    amenityDescription: '',
+    subheader: 'Bed and bath',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Hangers',
+    amenityDescription: '',
+    subheader: 'Bed and bath',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: '',
+    amenityDescription: '',
+    subheader: 'Veritatis quo architecto quibusdam.',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Hot water',
+    amenityDescription: '',
+    subheader: 'Essentials',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Shower gel',
+    amenityDescription: '',
+    subheader: 'Bed and bath',
+  },
+  {
+    iconLink: 'https://picsum.photos/25',
+    title: 'Wifi',
+    amenityDescription: '',
+    subheader: 'Basic',
+  },
+];
+
+export { sampleDescriptions, amenities };
