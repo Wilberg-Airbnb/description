d5d1d84b1190842b6b9517c3e2ce8d4e6eb3bc7e
Eric Zepeda <zepedaeric@gmail.com>
2020-07-15 21:46:33 -0700

    Dockerize service

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-
-const mongoUri = 'mongodb://localhost/fec-descriptions';
-const db = mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Have to figure out a way to switch between localhost (local)
+// and mongo (dockerizing)
+const mongoUri = 'mongodb://mongo:27017/fec-descriptions';
+const db = mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to mongo!'))
+  .catch((error) => console.log('Error connecting here:', error));
 
 module.exports = db;
