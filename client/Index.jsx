6ad95490acbb1c52c47a036ecbf912558d837998
Eric Zepeda <zepedaeric@gmail.com>
2020-07-21 18:46:21 -0700

    Change path name to work for windows

--- client/Index.jsx
+++ client/Index.jsx
@@ -28,8 +28,11 @@ class Index extends React.Component {
     // Have to add HTTP protocol && server port as per CORS policy
     // And data-fetching from the proxy-side
     // Switch between http://localhost:4000/api and /api.....
+    console.log('LISTINGID', this.state.listingId);
     axios
-      .get(`/api/description/${this.state.listingId}`)
+      .get(
+        `http://${window.location.host}/api/description/${this.state.listingId}`
+      )
       .then(({ data }) => {
         this.setState({
           descriptions: data.description,
