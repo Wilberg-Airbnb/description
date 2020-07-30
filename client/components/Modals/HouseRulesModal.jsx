9c15b4b9417f06e13d177694cd9078bdc0d1d2d2
Eric Zepeda <zepedaeric@gmail.com>
2020-07-29 20:09:14 -0700

    Modal Specificity

--- client/components/Modals/HouseRulesModal.jsx
+++ client/components/Modals/HouseRulesModal.jsx
@@ -3,70 +3,86 @@ import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const ModalBackgroundStyle = styled.div`
-  position: fixed;
-  padding: 0;
-  margin: 0;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
+  #description-modal-root & {
+    position: fixed;
+    padding: 0;
+    margin: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+  }
 `;
 
 const ModalStyle = styled.div`
-  padding: 20;
-  background: #fff;
-  border-radius: 8px;
-  display: inline-block;
-  height: 550px;
-  border-radius: 15px;
-  margin: 1rem;
-  position: relative;
-  min-width: 650px;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  justify-self: center;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
+  #description-modal-root & {
+    padding: 20;
+    background: #fff;
+    border-radius: 8px;
+    display: inline-block;
+    height: 550px;
+    border-radius: 15px;
+    margin: 1rem;
+    position: relative;
+    min-width: 650px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    justify-self: center;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+  }
 `;
 const RulesContainer = styled.div`
-  width: 100%;
-  height: 90%;
-  display: flex;
-  flex-direction: column;
+  #description-modal-root & {
+    width: 100%;
+    height: 90%;
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const HouseRulesHeader = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-  margin-left: 20px;
+  #description-modal-root & {
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 20px;
+  }
 `;
 const Url = 'https://rpt21-airbrb-description.s3-us-west-1.amazonaws.com/';
 
 const Paragaraph = styled.p`
-  font-size: 12px;
-  margin-bottom: 1px;
+  #description-modal-root & {
+    font-size: 12px;
+    margin-bottom: 1px;
+  }
 `;
 
 const AdditionalRulesHeader = styled.div`
-  font-size: 17px;
-  font-weight: bold;
-  margin-left: 20px;
-  margin-top: 30px;
+  #description-modal-root & {
+    font-size: 17px;
+    font-weight: bold;
+    margin-left: 20px;
+    margin-top: 30px;
+  }
 `;
 const RulesListContainer = styled.div`
-  margin-top: 10px;
-  display: flex;
-  width: 30%;
-  flex-direction: column;
-  margin-left: 20px;
+  #description-modal-root & {
+    margin-top: 10px;
+    display: flex;
+    width: 30%;
+    flex-direction: column;
+    margin-left: 20px;
+  }
 `;
 
 const AdditionalRulesParagraph = styled.div`
+  #description-modal-root & {
+  }
   display: flex;
   margin-left: 20px;
   font-size: 12px;
